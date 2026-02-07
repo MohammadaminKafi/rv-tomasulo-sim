a38eea9807859b9ab06f2c911f7872f06dad83eb
iamin <d.aminkafi@gmail.com>
2026-02-07 18:39:46 +0330

    minor fix

--- src/ui/simulatorPage.ts
+++ src/ui/simulatorPage.ts
@@ -64,7 +64,7 @@ export class SimulatorPage {
   private specEventLogDiv!: HTMLElement;
   private nonSpecEventLogSection!: HTMLElement;
 
-  constructor(router: Router) {
+  constructor(_router: Router) {
     this.simulator = new Simulator();
     this.codeEditor = null;
     this.isRunning = false;
